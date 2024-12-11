42bb1ebd527aa95ce7c01eb667db76e680479f9a
이동수 <dongsu6666@naver.com>
2024-12-11 14:46:15 +0900

    stock add-service 작성

--- src/main/java/com/nhnacademy/taskapi/stock/domain/Stock.java
+++ src/main/java/com/nhnacademy/taskapi/stock/domain/Stock.java
@@ -7,11 +7,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
