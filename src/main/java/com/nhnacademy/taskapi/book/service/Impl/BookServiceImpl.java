051e8d04171b68cdf8fb7855f51554d14eebb816
이동수 <dongsu6666@naver.com>
2024-12-11 16:51:32 +0900

    알라딘 api 적용 진행중

--- src/main/java/com/nhnacademy/taskapi/book/service/Impl/BookServiceImpl.java
+++ src/main/java/com/nhnacademy/taskapi/book/service/Impl/BookServiceImpl.java
@@ -1,19 +1,60 @@
 package com.nhnacademy.taskapi.book.service.Impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.taskapi.book.domain.Book;
 import com.nhnacademy.taskapi.book.repository.BookRepository;
 import com.nhnacademy.taskapi.book.service.BookService;
 import com.nhnacademy.taskapi.dto.BookSaveDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
+
+    // 신간 리스트 50개 정보 받아오기
     @Override
-    public Book SaveBook(BookSaveDTO bookSaveDTO) {
+    @Transactional
+    public void saveBookFromAladin() {
+        List<Book> bookList = new ArrayList<>();
+        String url = "https://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbtjswns12211534001&QueryType=ItemNewAll&MaxResults=50&start=1&SearchTarget=Book&output=js&Version=20131101";
+        RestTemplate restTemplate = new RestTemplate();
+
+        String response = restTemplate.getForObject(url, String.class);
+
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response);
+            JsonNode itemNode = rootNode.path("item");
+
+            for(JsonNode item : itemNode) {
+                item.path("title").asText();
+                item.
+            }
+
+
+
+
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public Book saveBook(BookSaveDTO bookSaveDTO) {
         Book book = new Book();
 
         book.setTitle(bookSaveDTO.getTitle());
