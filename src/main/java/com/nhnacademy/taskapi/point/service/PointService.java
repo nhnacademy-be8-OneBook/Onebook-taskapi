0a2ee225a292f551214377c0fa74fff2a886f5a0
ParkSuYeon <108243750+tndus165@users.noreply.github.com>
2024-12-11 20:45:51 -0800

    Update PointService.java

--- src/main/java/com/nhnacademy/taskapi/point/service/PointService.java
+++ src/main/java/com/nhnacademy/taskapi/point/service/PointService.java
@@ -5,7 +5,7 @@ import com.nhnacademy.taskapi.point.response.PointResponse;
 import com.nhnacademy.taskapi.point.response.UpdatePointResponse;
 
 public interface PointService {
-    PointResponse findPointByUserId(Long userId);
-    // 구매시 포인트 업데이트하고 현재 포인트 반환
-    UpdatePointResponse updatePointByUserId(Long userId, UpdatePointRequest pointRequest);
+    // 구매시 포인트 업데이트 후, 현재 포인트 반환
+    UpdatePointResponse updatePointByUserId(Long member_id, UpdatePointRequest pointRequest);
+    PointResponse findPointByUserId(Long member_id);
 }
