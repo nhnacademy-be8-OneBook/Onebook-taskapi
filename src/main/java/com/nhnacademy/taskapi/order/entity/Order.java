dafd798b39759860537cc506e13cc4acfedeec02
kimseonj <kimsunjun1221@gmail.com>
2024-12-12 11:21:02 +0900

    test

--- src/main/java/com/nhnacademy/taskapi/order/entity/Order.java
+++ src/main/java/com/nhnacademy/taskapi/order/entity/Order.java
@@ -1,11 +1,16 @@
 package com.nhnacademy.taskapi.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.nhnacademy.taskapi.customer.domain.Customer;
+import jakarta.persistence.*;
 
+@Table(name = "order")
 @Entity
 public class Order {
     @Id
+    @Column(name = "order_id")
     Long orderId;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    Customer customer;
 }
