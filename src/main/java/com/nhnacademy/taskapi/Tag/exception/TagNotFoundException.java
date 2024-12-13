ddfc287d09bda1b3eb61c50796fd2cab30c43612
이동수 <dongsu6666@naver.com>
2024-12-13 16:46:01 +0900

    출판사 not found, 태그 not found 예외 작성

--- src/main/java/com/nhnacademy/taskapi/Tag/exception/TagNotFoundException.java
+++ src/main/java/com/nhnacademy/taskapi/Tag/exception/TagNotFoundException.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.taskapi.Tag.exception;
 
-import jakarta.ws.rs.NotFoundException;
 
 public class TagNotFoundException extends RuntimeException {
     public TagNotFoundException(String message) {
--- src/main/java/com/nhnacademy/taskapi/publisher/exception/PublisherNotFoundException.java
+++ src/main/java/com/nhnacademy/taskapi/publisher/exception/PublisherNotFoundException.java
@@ -2,7 +2,7 @@ package com.nhnacademy.taskapi.publisher.exception;
 
 import jakarta.ws.rs.NotFoundException;
 
-public class PublisherNotFoundException extends NotFoundException {
+public class PublisherNotFoundException extends RuntimeException {
     public PublisherNotFoundException(String message) {
         super(message);
     }
