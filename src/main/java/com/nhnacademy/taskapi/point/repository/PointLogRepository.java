d1e1badffb03bac2d1f13da9df441dd3b775ba27
ParkSuYeon <tndus165@naver.com>
2024-12-13 10:02:03 -0800

    add point jpa

--- src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
+++ src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
@@ -4,5 +4,4 @@ import com.nhnacademy.taskapi.point.domain.PointLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointLogRepository extends JpaRepository<PointLog, Long> {
-
 }
