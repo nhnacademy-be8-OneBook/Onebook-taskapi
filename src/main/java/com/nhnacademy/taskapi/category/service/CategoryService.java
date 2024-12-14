4a59f29c15c7047fa62445d8387b915411bf675d
이동수 <dongsu6666@naver.com>
2024-12-14 17:58:53 +0900

    카테고리 등록 관련 예외 작성

--- src/main/java/com/nhnacademy/taskapi/category/service/CategoryService.java
+++ src/main/java/com/nhnacademy/taskapi/category/service/CategoryService.java
@@ -3,5 +3,5 @@ package com.nhnacademy.taskapi.category.service;
 import com.nhnacademy.taskapi.category.domain.Category;
 
 public interface CategoryService {
-    Category addCategory(int categoryId, String categoryName);
+    Category addCategory(String categoryName, Category parentCategory);
 }
