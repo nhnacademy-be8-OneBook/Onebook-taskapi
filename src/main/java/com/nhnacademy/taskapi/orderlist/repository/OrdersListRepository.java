891ad4673c786136728516f547ca30c67087385a
Seonjun Kim <97016371+kimseonj@users.noreply.github.com>
2024-12-16 09:41:57 +0900

    remove file

--- src/main/java/com/nhnacademy/taskapi/orderlist/repository/OrdersListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.taskapi.orderlist.repository;
-
-import com.nhnacademy.taskapi.orderlist.entity.OrdersList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersListRepository extends JpaRepository<OrdersList, Long> {
-}
