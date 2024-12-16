92d0f59712d8594cf565c781050c1696313ede91
kimseonj <kimsunjun1221@gmail.com>
2024-12-16 09:57:00 +0900

    update point

--- src/main/java/com/nhnacademy/taskapi/point/request/UpdateRefundRequest.java
+++ src/main/java/com/nhnacademy/taskapi/point/request/UpdateRefundRequest.java
@@ -3,7 +3,6 @@ package com.nhnacademy.taskapi.point.request;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
 @Builder
 public record UpdateRefundRequest(int refundAmount) {
     public boolean isRefundAmountValid() {
