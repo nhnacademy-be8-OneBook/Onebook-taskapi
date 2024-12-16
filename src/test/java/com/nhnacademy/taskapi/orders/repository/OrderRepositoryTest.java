112e6174179094c3a3c82e6939f2cabe5065b0de
kimseonj <kimsunjun1221@gmail.com>
2024-12-16 10:29:53 +0900

    update orders -> order

--- src/test/java/com/nhnacademy/taskapi/orders/repository/OrderRepositoryTest.java
+++ src/test/java/com/nhnacademy/taskapi/orders/repository/OrderRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.taskapi.orders.repository;
 
-import com.nhnacademy.taskapi.orders.dto.SaveOrders;
-import com.nhnacademy.taskapi.orders.entity.Orders;
+import com.nhnacademy.taskapi.order.entity.Order;
+import com.nhnacademy.taskapi.order.repository.OrderRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
@@ -30,59 +30,59 @@ class OrderRepositoryTest {
 //        log.info("setup");
 //    }
 
-    // create
-    @Test
-    void save() {
-        log.info("order DB 넣기 전 count !!!!! : {}", orderRepository.findAll().isEmpty());
-
-        Orders orders = orderRepository.save(new Orders(
-                "010-9999-9999", LocalDateTime.now(), 3000, 25000)
-        );
-
-        log.info("order DB 넣은 후 count !!!!! : {}", orderRepository.findAll().isEmpty());
-        entityManager.flush();
-
-        List<Orders> findOrdersList = orderRepository.findByPhoneNumber("010-9999-9999");
-        System.out.println("findOrderList !!!!!!");
-        findOrdersList.forEach(x -> System.out.println(x.toString()));
-    }
-
-    // read
-    @Test
-    void findOrder() {
-        Long id = 1L;
-        Orders orders = orderRepository.findById(id).orElse(null);
-
-        Assertions.assertThat(orders).isNotNull();
-        Assertions.assertThat(orders.getPhoneNumber()).isEqualTo("010-1111-1111");
-        Assertions.assertThat(orders.getDeliveryPrice()).isEqualTo(3000);
-        Assertions.assertThat(orders.getTotalPrice()).isEqualTo(25000);
-    }
-
-    // update
-    @Sql("/repository/order-test.sql")
-    @Test
-    void updateOrders() {
-        Long id = 1L;
-        Orders orders = orderRepository.findById(id).orElse(null);
-        orders.setPhoneNumber("010-1234-1234");
-        orders.setTotalPrice(5000);
-        orderRepository.saveAndFlush(orders);
-
-        Orders orders1 = orderRepository.findById(id).orElse(null);
-        Assertions.assertThat(orders).isNotNull();
-        Assertions.assertThat(orders1.getPhoneNumber()).isEqualTo("010-1234-1234");
-        Assertions.assertThat(orders1.getTotalPrice()).isEqualTo(5000);
-    }
-
-    // delete
-    @Sql("/repository/order-test.sql")
-    @Test
-    void deleteOrders() {
-        Long id = 1L;
-        orderRepository.deleteById(id);
-
-        Orders orders = orderRepository.findById(id).orElse(null);
-        Assertions.assertThat(orders).isNull();
-    }
+//    // create
+//    @Test
+//    void save() {
+//        log.info("order DB 넣기 전 count !!!!! : {}", orderRepository.findAll().isEmpty());
+//
+//        Order order = orderRepository.save(new Order(
+//                "010-9999-9999", LocalDateTime.now(), 3000, 25000)
+//        );
+//
+//        log.info("order DB 넣은 후 count !!!!! : {}", orderRepository.findAll().isEmpty());
+//        entityManager.flush();
+//
+//        List<Order> findOrderList = orderRepository.findByPhoneNumber("010-9999-9999");
+//        System.out.println("findOrderList !!!!!!");
+//        findOrderList.forEach(x -> System.out.println(x.toString()));
+//    }
+//
+//    // read
+//    @Test
+//    void findOrder() {
+//        Long id = 1L;
+//        Order order = orderRepository.findById(id).orElse(null);
+//
+//        Assertions.assertThat(order).isNotNull();
+//        Assertions.assertThat(order.getPhoneNumber()).isEqualTo("010-1111-1111");
+//        Assertions.assertThat(order.getDeliveryPrice()).isEqualTo(3000);
+//        Assertions.assertThat(order.getTotalPrice()).isEqualTo(25000);
+//    }
+//
+//    // update
+//    @Sql("/repository/order-test.sql")
+//    @Test
+//    void updateOrder() {
+//        Long id = 1L;
+//        Order order = orderRepository.findById(id).orElse(null);
+//        order.setPhoneNumber("010-1234-1234");
+//        order.setTotalPrice(5000);
+//        orderRepository.saveAndFlush(order);
+//
+//        Order order1 = orderRepository.findById(id).orElse(null);
+//        Assertions.assertThat(order).isNotNull();
+//        Assertions.assertThat(order1.getPhoneNumber()).isEqualTo("010-1234-1234");
+//        Assertions.assertThat(order1.getTotalPrice()).isEqualTo(5000);
+//    }
+//
+//    // delete
+//    @Sql("/repository/order-test.sql")
+//    @Test
+//    void deleteOrder() {
+//        Long id = 1L;
+//        orderRepository.deleteById(id);
+//
+//        Order order = orderRepository.findById(id).orElse(null);
+//        Assertions.assertThat(order).isNull();
+//    }
 }
