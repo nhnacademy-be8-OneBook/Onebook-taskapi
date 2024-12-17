fb03f70a01fe5cd6d5380f1f03657a7e82d79a27
ParkSuYeon <tndus165@naver.com>
2024-12-17 11:12:17 -0800

    point Authorization 삭제

--- src/main/java/com/nhnacademy/taskapi/member/service/MemberService.java
+++ src/main/java/com/nhnacademy/taskapi/member/service/MemberService.java
@@ -3,15 +3,17 @@ package com.nhnacademy.taskapi.member.service;
 import com.nhnacademy.taskapi.member.domain.Member;
 import com.nhnacademy.taskapi.member.dto.MemberLoginDto;
 import com.nhnacademy.taskapi.member.dto.MemberRegisterDto;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
 public interface MemberService {
     List<Member> getAllMembers();
     Member getMemberById(String id);
-    Member getMemberByLoginId(String loginId);
+//    Member getMemberByLoginId(String loginId);
     boolean existsById(String id);
     boolean existsByLoginId(String loginId);
-    boolean checkMember(MemberLoginDto memberLoginDto);
+    MemberLoginDto checkMemberLogin(MemberLoginDto memberLoginDto);
     Member registerMember(MemberRegisterDto memberRegisterDto);
+    Member getAuthenticatedMember(HttpServletRequest request); // 인증된 사용자 정보를 가져오는 메서드
 }
