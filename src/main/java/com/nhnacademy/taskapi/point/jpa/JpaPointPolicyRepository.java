72ebc7d6394600fa2bbb3c4a00ff940c20871a58
kimseonj <kimsunjun1221@gmail.com>
2024-12-17 13:28:37 +0900

    Revert "Update JpaPointPolicyRepository.java"
    This reverts commit d92981117d80fbbe1d42812d5a7a0ac04a195512.

--- src/main/java/com/nhnacademy/taskapi/point/jpa/JpaPointPolicyRepository.java
+++ src/main/java/com/nhnacademy/taskapi/point/jpa/JpaPointPolicyRepository.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.taskapi.point.jpa;
 
-import com.nhnacademy.taskapi.point.domain.PointLog;
+import com.nhnacademy.taskapi.point.domain.PointPolicy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaPointRepository extends JpaRepository<PointLog, String> {
-    Page<PointLog> findByPoint_Member_IdOrderByPointUpdatedAtDesc(String member_id, Pageable pageable);
+public interface JpaPointPolicyRepository extends JpaRepository<PointPolicy, String> {
+    PointPolicy findByPointPolicyName(String pointPolicyName);
+    Page<PointPolicy> findAllBy(Pageable pageable);
+    Page<PointPolicy> findAllByOrderByPointPolicyCreatedAtAscPointPolicyStateDesc(Pageable pageable);
 }
