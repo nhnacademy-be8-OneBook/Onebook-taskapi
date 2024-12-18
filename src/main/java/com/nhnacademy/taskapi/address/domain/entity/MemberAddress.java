62fb8c27e5198a239409f16a2a526b06516f4298
pangpange <eric0420@naver.com>
2024-12-18 18:02:41 +0900

    feat/배송지 등록,수정,조회,삭제

--- src/main/java/com/nhnacademy/taskapi/address/domain/entity/MemberAddress.java
+++ src/main/java/com/nhnacademy/taskapi/address/domain/entity/MemberAddress.java
@@ -14,33 +14,52 @@ import lombok.Setter;
 public class MemberAddress {
 
     @Id
-    @GeneratedValue
-    @Column(name = "member_address_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_address_id", nullable = false)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     @Setter
+    @Column(nullable = false ,length = 10)
     private String name;
+
     @Setter
+    @Column(nullable = false, length = 30)
     private String phoneNumber;
+
     @Setter
+    @Column(length = 20)
     private String alias;
+
     @Setter
+    @Column(length = 100)
     private String requestedTerm;
+
     @Setter
+    @Column(nullable = false,length = 10)
     private String zipCode;
+
     @Setter
+    @Column(nullable = false,length = 100)
     private String roadNameAddress;
+
     @Setter
+    @Column(nullable = false,length = 100)
     private String numberAddress;
+
     @Setter
+    @Column(length = 100)
     private String notes;
+
     @Setter
+    @Column(length = 100)
     private String detailAddress;
+
     @Setter
+    @Column(nullable = false)
     private Integer defaultLocation;
 
 }
