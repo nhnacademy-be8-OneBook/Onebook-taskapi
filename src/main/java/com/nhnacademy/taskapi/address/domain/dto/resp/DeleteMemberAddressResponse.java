29a98611b68c37344167e7ef03de439325e85f2a
pangpange <eric0420@naver.com>
2024-12-18 17:28:15 +0900

    feat/배송지 등록,수정,조회,삭제

--- src/main/java/com/nhnacademy/taskapi/address/domain/dto/resp/DeleteMemberAddressResponse.java
+++ src/main/java/com/nhnacademy/taskapi/address/domain/dto/resp/DeleteMemberAddressResponse.java
@@ -1,5 +1,10 @@
 package com.nhnacademy.taskapi.address.domain.dto.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class DeleteMemberAddressResponse {
     private Long id;
 }
