2965e79cc5504a0a1dfd5eadd71b0469142b0595
이동수 <dongsu6666@naver.com>
2024-12-18 15:07:24 +0900

    출판사 이름 예외 작성

---
+++ src/main/java/com/nhnacademy/taskapi/publisher/exception/InvalidPublisherNameException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.taskapi.publisher.exception;
+
+public class InvalidPublisherNameException extends RuntimeException {
+    public InvalidPublisherNameException(String message) {
+        super(message);
+    }
+}
