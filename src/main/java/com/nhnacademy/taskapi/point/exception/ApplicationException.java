9dc99de2db0b48c57e5e3d40fdc7d4119d3704ec
ParkSuYeon <tndus165@naver.com>
2024-12-17 17:47:13 -0800

    create point

---
+++ src/main/java/com/nhnacademy/taskapi/point/exception/ApplicationException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.taskapi.point.exception;
+
+public abstract class ApplicationException extends RuntimeException {
+    public ApplicationException(String message) {
+        super(message);  // 부모 클래스에서 메시지를 처리
+    }
+}
