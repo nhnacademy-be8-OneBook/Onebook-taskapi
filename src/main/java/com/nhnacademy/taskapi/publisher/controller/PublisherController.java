7d4471bc1f505794417b408ea76b155013189270
이동수 <dongsu6666@naver.com>
2024-12-18 15:07:57 +0900

    출판사 수정, 삭제 & 컨트롤러 작성

---
+++ src/main/java/com/nhnacademy/taskapi/publisher/controller/PublisherController.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.taskapi.publisher.controller;
+
+import com.nhnacademy.taskapi.publisher.domain.Publisher;
+import com.nhnacademy.taskapi.publisher.dto.PublisherUpdateDTO;
+import com.nhnacademy.taskapi.publisher.service.PublisherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/publisher")
+public class PublisherController {
+    private final PublisherService publisherService;
+
+
+    @PostMapping("/{puhlisherName}")
+    public ResponseEntity<Publisher> createPublisher(@PathVariable String puhlisherName){
+        Publisher publisher = publisherService.addPublisher(puhlisherName);
+        return ResponseEntity.ok().body(publisher);
+    }
+
+    @PutMapping
+    public ResponseEntity<Publisher> modifyPublisher(@RequestBody PublisherUpdateDTO dto){
+        Publisher publisher = publisherService.updatePublisher(dto);
+        return ResponseEntity.ok().body(publisher);
+    }
+
+
+    @DeleteMapping("/{publisherId}")
+    public ResponseEntity<Void> deletePublisher(long publisherId){
+        publisherService.deletePublisher(publisherId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
--- src/main/java/com/nhnacademy/taskapi/publisher/service/serviceImpl/PublisherServiceImpl.java
+++ src/main/java/com/nhnacademy/taskapi/publisher/service/serviceImpl/PublisherServiceImpl.java
@@ -1,11 +1,16 @@
 package com.nhnacademy.taskapi.publisher.service.serviceImpl;
 
 import com.nhnacademy.taskapi.publisher.domain.Publisher;
+import com.nhnacademy.taskapi.publisher.dto.PublisherUpdateDTO;
+import com.nhnacademy.taskapi.publisher.exception.InvalidPublisherNameException;
+import com.nhnacademy.taskapi.publisher.exception.PublisherNotFoundException;
 import com.nhnacademy.taskapi.publisher.repository.PublisherRepository;
 import com.nhnacademy.taskapi.publisher.service.PublisherService;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,25 @@ public class PublisherServiceImpl implements PublisherService {
         Publisher publisher = new Publisher();
         publisher.setName(name);
         return publisherRepository.save(publisher);
+    }
+
+    @Override
+    public Publisher updatePublisher(PublisherUpdateDTO dto){
+        Publisher publisher = publisherRepository.findById(dto.getPublisherId()).orElseThrow(() -> new PublisherNotFoundException("This Publisher Not Exist !"));
+
+        if(Objects.isNull(dto.getPublisherName()) || dto.getPublisherName().trim().isEmpty()){
+            throw new InvalidPublisherNameException("This PublisherName is Null Or Empty");
+        }
+
+        publisher.setName(dto.getPublisherName());
+
+        return publisherRepository.save(publisher);
+    }
+
+    @Override
+    public void deletePublisher(long publisherId){
+        Publisher publisher = publisherRepository.findById(publisherId).orElseThrow(() -> new PublisherNotFoundException("This Publisher Not Exist !"));
+        publisherRepository.delete(publisher);
     }
 
 
