a92cec253161f97be711ac666ebf968012c9bf10
이동수 <dongsu6666@naver.com>
2024-12-18 11:18:44 +0900

    작가 수정, 삭제 기능 추가 & 컨트롤러 작성

--- src/main/java/com/nhnacademy/taskapi/author/controller/AuthorController.java
+++ src/main/java/com/nhnacademy/taskapi/author/controller/AuthorController.java
@@ -1,15 +1,35 @@
 package com.nhnacademy.taskapi.author.controller;
 
 
+import com.nhnacademy.taskapi.author.domain.Author;
+import com.nhnacademy.taskapi.author.dto.AuthorUpdateDTO;
 import com.nhnacademy.taskapi.author.service.AuthorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/author")
 public class AuthorController {
     private final AuthorService authorService;
 
+    @PostMapping("/{authorName}")
+    public ResponseEntity<Author> createAuthor(@PathVariable String authorName){
+        Author author = authorService.addAuthor(authorName);
+        return ResponseEntity.ok().body(author);
+    }
+
+    @PutMapping
+    public ResponseEntity<Author> modifyAuthor(@RequestBody AuthorUpdateDTO dto){
+        Author author = authorService.updateAuthor(dto);
+        return ResponseEntity.ok().body(author);
+    }
+
+    @DeleteMapping("/{authorId}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable int authorId){
+        authorService.deleteAuthor(authorId);
+        return ResponseEntity.noContent().build();
+    }
 
 }
