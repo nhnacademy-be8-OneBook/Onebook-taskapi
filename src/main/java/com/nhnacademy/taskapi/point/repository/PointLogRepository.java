cfb89bea937a1f8ba617b15c9ebf43cbc6d2f286
ParkSuYeon <tndus165@naver.com>
2024-12-17 16:48:19 -0800

    point Authorization 삭제

--- src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
+++ src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
@@ -3,4 +3,5 @@ package com.nhnacademy.taskapi.point.repository;
 import com.nhnacademy.taskapi.point.domain.PointLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointLogRepository extends JpaRepository<PointLog, String> { }
+public interface PointLogRepository extends JpaRepository<PointLog, String> {
+}
