dcf053b672c9e56cde0ec41ddbf16999bf5793b8
Joo-v7 <helloworld.v7@gmail.com>
2024-12-19 15:12:30 +0900

    로그인ID를 return하는 기능 추가.

--- src/main/java/com/nhnacademy/taskapi/member/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/taskapi/member/repository/MemberRepository.java
@@ -2,10 +2,15 @@ package com.nhnacademy.taskapi.member.repository;
 
 import com.nhnacademy.taskapi.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
+
+    @Query("SELECT m.loginId FROM Member m WHERE m.id = :id")
+    Optional<String> getLoginIdById(Long id);
+
 }
