98702da7049396713bdb01e7e984f9d4bec963c3
Joo-v7 <helloworld.v7@gmail.com>
2024-12-20 09:12:01 +0900

    Grade, Role Test 코드 추가.

---
+++ src/test/java/com/nhnacademy/taskapi/grade/controller/GradeControllerTest.java
@@ -0,0 +1,99 @@
+package com.nhnacademy.taskapi.grade.controller;
+
+import com.nhnacademy.taskapi.grade.domain.Grade;
+import com.nhnacademy.taskapi.grade.dto.GradeModifyDto;
+import com.nhnacademy.taskapi.grade.service.GradeService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(controllers = GradeController.class)
+public class GradeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GradeService gradeService;
+
+    @Test
+    @DisplayName("GET All Grades")
+    void getAllTest() throws Exception {
+        List<Grade> gradeList = Arrays.asList(Grade.create("REGULAR", 1, "test"));
+
+        Mockito.when(gradeService.getAllGrades()).thenReturn(gradeList);
+
+        mockMvc.perform(get("/task/grades"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("REGULAR"))
+                .andExpect(jsonPath("$[0].accumulationRate").value(1))
+                .andExpect(jsonPath("$[0].description").value("test"));
+    }
+
+    @Test
+    @DisplayName("GET One Grade")
+    void getOneTest() throws Exception {
+        Grade grade = Grade.create("REGULAR", 1, "test");
+
+        Mockito.when(gradeService.getGradeById(Mockito.anyInt())).thenReturn(grade);
+
+        mockMvc.perform(get("/task/grades/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("REGULAR"))
+                .andExpect(jsonPath("$.accumulationRate").value(1))
+                .andExpect(jsonPath("$.description").value("test"));
+    }
+
+    @Test
+    @DisplayName("POST Grade")
+    void postTest() throws Exception {
+        Grade grade = Grade.create("REGULAR", 1, "test");
+
+        Mockito.when(gradeService.registerGrade(Mockito.any())).thenReturn(grade);
+
+        mockMvc.perform(post("/task/grades")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"REGULAR\", \"accumulationRate\": 1, \"description\": \"test\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("REGULAR"))
+                .andExpect(jsonPath("$.accumulationRate").value(1))
+                .andExpect(jsonPath("$.description").value("test"));
+    }
+
+    @Test
+    @DisplayName("PUT Grade")
+    void putTest() throws Exception {
+        Grade grade = Grade.create("TEST", 2, "TEST");
+
+        Mockito.when(gradeService.modifyGrade(1, new GradeModifyDto("TEST", 2, "TEST"))).thenReturn(grade);
+
+        mockMvc.perform(put("/task/grades/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"TEST\", \"accumulationRate\": 2, \"description\": \"TEST\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("TEST"))
+                .andExpect(jsonPath("$.accumulationRate").value(2))
+                .andExpect(jsonPath("$.description").value("TEST"));
+    }
+
+    @Test
+    @DisplayName("DELETE Grade")
+    void deleteTest() throws Exception {
+        mockMvc.perform(delete("/task/grades/1"))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
---
+++ src/test/java/com/nhnacademy/taskapi/roles/controller/RoleControllerTest.java
@@ -0,0 +1,99 @@
+package com.nhnacademy.taskapi.roles.controller;
+
+import com.nhnacademy.taskapi.roles.domain.Role;
+import com.nhnacademy.taskapi.roles.dto.RoleModifyDto;
+import com.nhnacademy.taskapi.roles.dto.RoleRegisterDto;
+import com.nhnacademy.taskapi.roles.service.RoleService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(controllers = RoleController.class)
+public class RoleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RoleService roleService;
+
+
+    @Test
+    @DisplayName("GET All Roles")
+    void getAllTest() throws Exception {
+        List<Role> roleList = Arrays.asList(Role.createRole("MEMBER", "일반 회원"));
+
+        Mockito.when(roleService.getAllRoles()).thenReturn(roleList);
+
+        mockMvc.perform(get("/task/roles"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("MEMBER"))
+                .andExpect(jsonPath("$[0].description").value("일반 회원"));
+    }
+
+    @Test
+    @DisplayName("GET One Role")
+    void getOneTest() throws Exception {
+        Role role = Role.createRole("MEMBER", "일반 회원");
+
+        Mockito.when(roleService.getRoleById(1)).thenReturn(role);
+
+        mockMvc.perform(get("/task/roles/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("MEMBER"))
+                .andExpect(jsonPath("$.description").value("일반 회원"));
+    }
+
+    @Test
+    @DisplayName("POST Role")
+    void createTest() throws Exception {
+        Role role = Role.createRole("MEMBER", "일반 회원");
+
+        Mockito.when(roleService.registerRole(new RoleRegisterDto("MEMBER", "일반 회원"))).thenReturn(role);
+
+        mockMvc.perform(post("/task/roles")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"MEMBER\",\"description\":\"일반 회원\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("MEMBER"))
+                .andExpect(jsonPath("$.description").value("일반 회원"));
+    }
+
+    @Test
+    @DisplayName("PUT Role")
+    void updateTest() throws Exception {
+        Role role = Role.createRole("ADMIN", "관리자");
+
+        Mockito.when(roleService.modifyRole(1, new RoleModifyDto("ADMIN", "관리자"))).thenReturn(role);
+
+        mockMvc.perform(put("/task/roles/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"ADMIN\", \"description\": \"관리자\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("ADMIN"))
+                .andExpect(jsonPath("$.description").value("관리자"));
+    }
+
+    @Test
+    @DisplayName("DELETE Role")
+    void deleteTest() throws Exception {
+        mockMvc.perform(delete("/task/roles/1"))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+}
