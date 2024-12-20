daf09e9f0c37a8b46ab9c1c27552f3fa985866af
이동수 <dongsu6666@naver.com>
2024-12-20 15:16:39 +0900

    AuthorNotFoundException - 상속을 NotFound -> Runtime 변경
    AuthorServiceImpl 테스트코드 작성

--- src/main/java/com/nhnacademy/taskapi/author/exception/AuthorNotFoundException.java
+++ src/main/java/com/nhnacademy/taskapi/author/exception/AuthorNotFoundException.java
@@ -2,6 +2,6 @@ package com.nhnacademy.taskapi.author.exception;
 
 import jakarta.ws.rs.NotFoundException;
 
-public class AuthorNotFoundException extends NotFoundException {
+public class AuthorNotFoundException extends RuntimeException {
     public AuthorNotFoundException(String message) {super(message);}
 }
