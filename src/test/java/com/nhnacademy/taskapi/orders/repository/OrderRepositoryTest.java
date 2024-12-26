8d1b154eccffcc16ec36aee29732f69c3d38510f
kimseonj <kimsunjun1221@gmail.com>
2024-12-26 14:11:39 +0900

    create OrderRepositoryTest

--- src/test/java/com/nhnacademy/taskapi/orders/repository/OrderRepositoryTest.java
+++ src/test/java/com/nhnacademy/taskapi/orders/repository/OrderRepositoryTest.java
@@ -1,88 +1,124 @@
 package com.nhnacademy.taskapi.orders.repository;
 
+import com.nhnacademy.taskapi.grade.domain.Grade;
+import com.nhnacademy.taskapi.member.domain.Member;
+import com.nhnacademy.taskapi.member.repository.MemberRepository;
 import com.nhnacademy.taskapi.order.entity.Order;
 import com.nhnacademy.taskapi.order.repository.OrderRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import com.nhnacademy.taskapi.roles.domain.Role;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @DataJpaTest
 class OrderRepositoryTest {
+
     @Autowired
     private OrderRepository orderRepository;
 
-    @PersistenceContext
-    private EntityManager entityManager;
-//
-//    @Sql("/repository/order-test.sql")
-//    @BeforeAll
-//    static void setup() {
-//        log.info("setup");
-//    }
+    @Autowired
+    private TestEntityManager entityManager;
+    @Autowired
+    private MemberRepository memberRepository;
 
-//    // create
-//    @Test
-//    void save() {
-//        log.info("order DB 넣기 전 count !!!!! : {}", orderRepository.findAll().isEmpty());
-//
-//        Order order = orderRepository.save(new Order(
-//                "010-9999-9999", LocalDateTime.now(), 3000, 25000)
-//        );
-//
-//        log.info("order DB 넣은 후 count !!!!! : {}", orderRepository.findAll().isEmpty());
-//        entityManager.flush();
-//
-//        List<Order> findOrderList = orderRepository.findByPhoneNumber("010-9999-9999");
-//        System.out.println("findOrderList !!!!!!");
-//        findOrderList.forEach(x -> System.out.println(x.toString()));
-//    }
-//
-//    // read
-//    @Test
-//    void findOrder() {
-//        Long id = 1L;
-//        Order order = orderRepository.findById(id).orElse(null);
-//
-//        Assertions.assertThat(order).isNotNull();
-//        Assertions.assertThat(order.getPhoneNumber()).isEqualTo("010-1111-1111");
-//        Assertions.assertThat(order.getDeliveryPrice()).isEqualTo(3000);
-//        Assertions.assertThat(order.getTotalPrice()).isEqualTo(25000);
-//    }
-//
-//    // update
-//    @Sql("/repository/order-test.sql")
-//    @Test
-//    void updateOrder() {
-//        Long id = 1L;
-//        Order order = orderRepository.findById(id).orElse(null);
-//        order.setPhoneNumber("010-1234-1234");
-//        order.setTotalPrice(5000);
-//        orderRepository.saveAndFlush(order);
-//
-//        Order order1 = orderRepository.findById(id).orElse(null);
-//        Assertions.assertThat(order).isNotNull();
-//        Assertions.assertThat(order1.getPhoneNumber()).isEqualTo("010-1234-1234");
-//        Assertions.assertThat(order1.getTotalPrice()).isEqualTo(5000);
-//    }
-//
-//    // delete
-//    @Sql("/repository/order-test.sql")
-//    @Test
-//    void deleteOrder() {
-//        Long id = 1L;
-//        orderRepository.deleteById(id);
-//
-//        Order order = orderRepository.findById(id).orElse(null);
-//        Assertions.assertThat(order).isNull();
-//    }
+    private Member member;
+
+    @BeforeEach
+    public void setUp() {
+        Grade grade = Grade.create("ROYAL", 10, "일반 등급");
+        Role role = Role.createRole("MEMBER", "일반 회원");
+
+        Grade saveGrade = entityManager.persist(grade);
+        Role saveRole = entityManager.persist(role);
+        this.member = Member.createNewMember(saveGrade, "김선준", "kimsj", "kimsj123", LocalDate.now(), Member.Gender.M, "kimsj@nhnacademy.com", "010-1111-2222", saveRole);
+        memberRepository.save(this.member);
+    }
+
+    // create
+    @Test
+    @DisplayName("Save order")
+    void save() {
+        // given
+        Order order = new Order(
+                this.member,
+                "김선준",
+                "010-9999-9999",
+                LocalDateTime.now(),
+                3000,
+                25000
+        );
+
+        // when: order 저장
+        Order saveOrder = orderRepository.save(order);
+
+        // then: 저장된 데이터 데이터 검증
+        Optional<Order> foundOrder = orderRepository.findById(saveOrder.getOrderId());
+        Assertions.assertTrue(foundOrder.isPresent());
+        Assertions.assertEquals(saveOrder.getOrderId(), foundOrder.get().getOrderId());
+        Assertions.assertEquals(saveOrder.getMember(), foundOrder.get().getMember());
+    }
+
+    // read
+    @Test
+    @DisplayName("Find Order By MemberId")
+    void findOrderByMemberId() {
+        // given
+        Order order = new Order(this.member, "김선준", "010-9999-9999", LocalDateTime.now(), 3000, 25000);
+        Order saveOrder = orderRepository.save(order);
+
+        // when
+        List<Order> allByMemberId = orderRepository.findAllByMemberId(this.member.getId());
+
+        // then
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, allByMemberId.size()),
+                () -> Assertions.assertEquals(saveOrder.getOrderId(), allByMemberId.get(0).getOrderId())
+        );
+    }
+
+    // update
+    @Test
+    void updateOrder() {
+        // given
+        Order order = new Order(this.member, "김선준", "010-9999-9999", LocalDateTime.now(), 3000, 25000);
+        Order saveOrder = orderRepository.save(order);
+        List<Order> allByMemberId = orderRepository.findAllByMemberId(this.member.getId());
+        Long orderId = allByMemberId.get(0).getOrderId();
+
+        // when
+        allByMemberId.get(0).setOrderer("김수정");
+
+        // then
+        Order findOrder = orderRepository.findById(orderId).orElse(null);
+
+        Assertions.assertNotNull(findOrder);
+        Assertions.assertEquals("김수정", findOrder.getOrderer());
+    }
+
+    // delete
+    @Test
+    void deleteOrder() {
+        // given
+        Order order = new Order(this.member, "김선준", "010-9999-9999", LocalDateTime.now(), 3000, 25000);
+        Order saveOrder = orderRepository.save(order);
+        List<Order> allByMemberId = orderRepository.findAllByMemberId(this.member.getId());
+        Long orderId = allByMemberId.get(0).getOrderId();
+
+        // when
+        orderRepository.deleteById(orderId);
+        List<Order> allByMemberId1 = orderRepository.findAllByMemberId(this.member.getId());
+
+        // then
+        Assertions.assertEquals(0, allByMemberId1.size());
+    }
 }
