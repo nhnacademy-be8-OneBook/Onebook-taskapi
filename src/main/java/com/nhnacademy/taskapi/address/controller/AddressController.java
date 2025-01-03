e2fe6de96e968dc12d4905de6a09999941327918
pangpange <eric0420@naver.com>
2025-01-03 10:45:51 +0900

    feat/memberAddressResponse 하나로 통일

--- src/main/java/com/nhnacademy/taskapi/address/controller/AddressController.java
+++ src/main/java/com/nhnacademy/taskapi/address/controller/AddressController.java
@@ -3,10 +3,7 @@ package com.nhnacademy.taskapi.address.controller;
 import com.nhnacademy.taskapi.address.domain.dto.req.AddMemberAddressRequest;
 import com.nhnacademy.taskapi.address.domain.dto.req.DeleteMemberAddressRequest;
 import com.nhnacademy.taskapi.address.domain.dto.req.UpdateMemberAddressRequest;
-import com.nhnacademy.taskapi.address.domain.dto.resp.AddMemberAddressResponse;
-import com.nhnacademy.taskapi.address.domain.dto.resp.DeleteMemberAddressResponse;
-import com.nhnacademy.taskapi.address.domain.dto.resp.GetMemberAddressResponse;
-import com.nhnacademy.taskapi.address.domain.dto.resp.UpdateMemberAddressResponse;
+import com.nhnacademy.taskapi.address.domain.dto.resp.MemberAddressResponse;
 import com.nhnacademy.taskapi.address.service.AddressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,38 +20,38 @@ public class AddressController {
     private final AddressService addressService;
 
     @PostMapping("/addresses")
-    public ResponseEntity<AddMemberAddressResponse> addMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId ,
-                                                                     @RequestBody AddMemberAddressRequest request){
+    public ResponseEntity<MemberAddressResponse> addMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId ,
+                                                                  @RequestBody AddMemberAddressRequest request){
 
-        AddMemberAddressResponse resp = addressService.addMemberAddress(memberId, request);
+        MemberAddressResponse resp = addressService.addMemberAddress(memberId, request);
         return new ResponseEntity<>(resp,HttpStatus.OK);
     }
 
     @GetMapping("/addresses")
-    public ResponseEntity<List<GetMemberAddressResponse>> getAllMemberAddressByMemberId(@RequestHeader("X-MEMBER-ID") Long memberId){
+    public ResponseEntity<List<MemberAddressResponse>> getAllMemberAddressByMemberId(@RequestHeader("X-MEMBER-ID") Long memberId){
 
-        List<GetMemberAddressResponse> resp = addressService.getMemberAddresses(memberId);
+        List<MemberAddressResponse> resp = addressService.getMemberAddresses(memberId);
         return new ResponseEntity<>(resp, HttpStatus.OK);
     }
 
     @GetMapping("/addresses/{addressId}")
-    public ResponseEntity<GetMemberAddressResponse> getMemberAddressById(@RequestHeader("X-MEMBER-ID") Long memberId, @PathVariable Long addressId ){
+    public ResponseEntity<MemberAddressResponse> getMemberAddressById(@RequestHeader("X-MEMBER-ID") Long memberId, @PathVariable Long addressId ){
 
-        GetMemberAddressResponse resp = addressService.getMemberAddress(memberId,addressId);
+        MemberAddressResponse resp = addressService.getMemberAddress(memberId,addressId);
         return new ResponseEntity<>(resp,HttpStatus.OK);
     }
     
     @PutMapping("/addresses")
-    public ResponseEntity<UpdateMemberAddressResponse> updateMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId, @RequestBody UpdateMemberAddressRequest request){
+    public ResponseEntity<MemberAddressResponse> updateMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId, @RequestBody UpdateMemberAddressRequest request){
 
-        UpdateMemberAddressResponse resp = addressService.updateMemberAddress(memberId, request);
+        MemberAddressResponse resp = addressService.updateMemberAddress(memberId, request);
         return new ResponseEntity<>(resp,HttpStatus.OK);
     }
 
     @DeleteMapping("/addresses")
-    public ResponseEntity<DeleteMemberAddressResponse> deleteMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId, @RequestBody DeleteMemberAddressRequest request){
+    public ResponseEntity<MemberAddressResponse> deleteMemberAddress(@RequestHeader("X-MEMBER-ID") Long memberId, @RequestBody DeleteMemberAddressRequest request){
 
-        DeleteMemberAddressResponse resp = addressService.deleteMemberAddress(memberId,request);
+        MemberAddressResponse resp = addressService.deleteMemberAddress(memberId,request);
         return new ResponseEntity<>(resp,HttpStatus.OK);
     }
 
