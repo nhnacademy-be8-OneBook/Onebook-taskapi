14be7cba9c51b1e004510c272147583659136d31
ParkSuYeon <tndus165@naver.com>
2025-01-07 13:34:03 +0900

    PointLogController JSON 구조 출력 /task 추가

--- src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
+++ src/main/java/com/nhnacademy/taskapi/point/repository/PointLogRepository.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.taskapi.point.repository;
 
 import com.nhnacademy.taskapi.point.domain.PointLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PointLogRepository extends JpaRepository<PointLog, String> {
+public interface PointLogRepository extends JpaRepository<PointLog, Long> {
+    Page<PointLog> findByPoint_Member_Id(Long memberId, Pageable pageable);
 }
